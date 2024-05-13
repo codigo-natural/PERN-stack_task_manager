e69ba1d319e36c37e7002bbba7322fcdfd5cd6a9
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-13 09:19:24 -0500

    feat ✨ add  endpoints for tasks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import morgan from "morgan"
 
+import taskRoutes from "./routes/tasks.routes.js"
+
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(taskRoutes)
 
 app.listen(3000)
 console.log("server on port 3000")
