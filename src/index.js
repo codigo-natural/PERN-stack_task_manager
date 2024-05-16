e7b7e8487809f3b3100818fb06a76535f511cd1e
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-16 12:30:54 -0500

    feat ✨  add connection with postgresql

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import express from "express"
-import morgan from "morgan"
+import express from "express";
+import morgan from "morgan";
 
-import taskRoutes from "./routes/tasks.routes.js"
+import taskRoutes from "./routes/tasks.routes.js";
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-app.use(taskRoutes)
+app.use(taskRoutes);
 
-app.listen(3000)
-console.log("server on port 3000")
+app.listen(3000);
+console.log("server on port 3000");
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,25 +1,25 @@
-import { Router} from "express"
+import { Router } from "express";
 
-const router = Router()
+const router = Router();
 
-router.get("/tasks", (req, res) => {
-  res.send("retrieving a lis of tasks")
-})
+router.get("/tasks", async (req, res) => {
+  res.send("retrieving all tasks");
+});
 
 router.get("/tasks/:id", (req, res) => {
-  res.send("retrieving a single task")
-})
+  res.send("retrieving a single task");
+});
 
 router.post("/tasks", (req, res) => {
-  res.send("creating a tasks")
-})
+  res.send("creating a tasks");
+});
 
 router.delete("/tasks/:id", (req, res) => {
-  res.send("deleting a task")
-})
+  res.send("deleting a task");
+});
 
 router.put("/tasks/:id", (req, res) => {
-  res.send("updating a task")
-})
+  res.send("updating a task");
+});
 
 export default router;
