9b4d85169634e907cc8c9ef931f975e579b14b9f
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-20 07:40:42 -0500

    design 🎨 add styles from navigation (Navbar)

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,10 +1,27 @@
-import { Button } from "@mui/material"
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material'
+import { Link, useNavigate } from "react-router-dom"
 
 export const Navbar = () => {
+  const navigate = useNavigate()
   return (
-    <div>
-      <div>Navbar</div>
-      <Button variant="outline">Click Me</Button>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color='transparent'>
+        <Container>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/" style={{ color: '#ffff' }}>Tasks App - Pern Stack</Link>
+            </Typography>
+            <Button
+              variant='outlined'
+              color='inherit'
+              style={{ color: '#fff' }}
+              onClick={() => navigate('/tasks/new')}
+            >
+              Create Task
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   )
 }
