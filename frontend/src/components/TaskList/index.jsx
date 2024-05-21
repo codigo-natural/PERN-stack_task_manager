046e96fd82b9ae9c0450a3b01979814ad80b7f73
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-05-21 18:03:48 -0500

    feat 🎉 list task with design

--- frontend/src/components/TaskList/index.jsx
+++ frontend/src/components/TaskList/index.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from "react"
+import { Button, Card, CardContent, Typography } from '@mui/material'
+
 export const Tasklist = () => {
+  const [tasks, setTasks] = useState([])
+  const loadTasks = async () => {
+    const response = await fetch('http://localhost:3000/tasks')
+    const data = await response.json()
+    setTasks(data)
+  }
+  useEffect(() => {
+    loadTasks()
+  }, [])
   return (
-    <div>Task list</div>
+    <>
+      <h1>Task list</h1>
+      {
+        tasks.map(task => (
+          <Card
+            style={{ marginBottom: '.7rem', backgroundColor: '#1e272e' }}
+            key={task.name}>
+            <CardContent style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <div style={{ color: 'white' }}>
+                <Typography>{task.title}</Typography>
+                <Typography>{task.Description}</Typography>
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  color="inherit"
+                  onClick={() => console.log('edit')}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  onClick={() => console.log('edit')}
+                  style={{ marginLeft: '.5rem' }}
+                >
+                  Delete
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))
+      }
+    </>
   )
 }
